5112daf94580ccc579295273c066eb5cff0c3998
Ying Qiao <yingqiao@eecs.berkeley.edu>
2013-02-27 20:20:09 -0800

    code done

--- ass3/ass3.r
+++ ass3/ass3.r
@@ -5,63 +5,85 @@
 rm(list=ls())
 
 #### Detective Work
-## N : total subject counts for various subgroups/margins
+#### N : total subject counts for various subgroups/margins
 unsub <- read.table("part6_907.txt", header=T)
 head(unsub)
+with(unsub, table(act))
 
-# --- from codebook (for reference here) ---
+# --- from codebook | for reference here ---
 # sempl:  0-unemployed;  1-employed;
-# act (treatment assigned | treatment delivered):
+# act:  treatment assigned | treatment delivered
 #         1-arr|arr;     2-non|non;    3-non|arr;   4-arr|non;
-
-# initial look
-with(unsub, table(sempl)) #table(unsub$sempl)["0"]
-table(unsub$act)
 
 # follow "intension-to-treat" to regroup "act"
 # trass: 0-no arrest;    1-arrest;
 unsub$trass <- as.numeric(unsub$act == 1 | unsub$act == 4)
-with(unsub, table(trass))
 
-# create N table and margins
-tbl <- with(unsub, table(sempl, trass))
-N.unempl <- sum(tbl[1, ])
-N.empl   <- sum(tbl[2, ])
-N.noarr  <- sum(tbl[ ,1])
-N.arr    <- sum(tbl[ ,2])
-N <- sum(sum(tbl))
+# create N table, margins can be derived
+N.tbl <- with(unsub, table(sempl, trass))
 
 # report the rate of unemployment among the subjects
-N.unempl / N  # PH gives 29%
+rowSums(N.tbl)[1] / sum(sum(N.tbl))   # PH gives 29% for unemployment rate
 
 
-## n : number of re-abusing subjects
+#### n : number of re-abusing subjects
 # --- from PH Figure 1.---
-PH.per <- cbind(c(7.1,12.3),c(16.7,6.2)) / 100 # following the same 0-1 in tbl
+PH.per <- cbind(c(7.1,12.3),c(16.7,6.2)) / 100 # following the same 0/1 in N.tbl
 
-# create n table and margins
-recid <- round(tbl*PH.per)
-n.unempl <- sum(recid[1, ])
-n.empl   <- sum(recid[2, ])
-n.noarr  <- sum(recid[ ,1])
-n.arr    <- sum(recid[ ,2])
-n <- sum(sum(recid))
+# Similarly, create n table, margins can be derived
+n.tbl <- round(N.tbl * PH.per)
 
-n.noarr / N.noarr # PH gives 10.6%
-n.arr / N.arr     # PH gives 9.0%
+# report the rate of re-abuse among non/-arrestees
+colSums(n.tbl)[1] / colSums(N.tbl)[1]  # PH gives 10.6% for non-arrestees
+colSums(n.tbl)[2] / colSums(N.tbl)[2]  # PH gives 9.0%  for arrestees
 
 
 #### Statistical Work
-# Fisher's exact test
+# a - Fisher's exact test
+# b - two sample test of equal binomial proportions
+#     z = (p1-p2)/sqrt(p(1-p)(1/n1+1/n2))
+#     where p=(n1p1+n2p2)/(n1+n2)
+two.binom.test <- function (tbl) {
+  # 2-by-2 table similar to Fisher test
+  p <- tbl[1, ] / colSums(tbl)
+  p.hat <- rowSums(tbl)[1] / sum(sum(tbl))
+  del.hat <- p[1] - p[2]
+  var.hat <- p.hat*(1-p.hat)*sum(1/colSums(tbl))
+  z.hat <- (del.hat) / sqrt(var.hat)
+  return (z.hat)
+}
 
 
-# 1. employed; recid ~ arrest
+# 1. employed; re-abuse ~ arrest
+# among employed subjects
+# H0: the occurrence of a subsequent assault is the same between the two groups
+# HA: the re-abuse rate is larger in the non-arrest group than the treatment group
+cl1 <- matrix(c(n.tbl[2, ], (N.tbl-n.tbl)[2, ]), nrow=2, byrow=T,
+              dimnames = list(c("reabuse","non-recid"), c("non-arrest","arrest")))
+cl1
+fisher.test(cl1, alternative="greater")$p.value
+1 - pnorm(two.binom.test(cl1))
 
 
+# 2. unemployed; re-abuse ~ arrest
+# among unemployed subjects
+# H0: the occurrence of a subsequent assault is the same between the two groups
+# HA: the re-abuse rate is smaller in the non-arrest group than the treatment group
+cl2 <- matrix(c(n.tbl[1, ], (N.tbl-n.tbl)[1, ]), nrow=2, byrow=T,
+              dimnames = list(c("reabuse","non-recid"), c("non-arrest","arrest")))
+cl2
+fisher.test(cl2, alternative="less")$p.value
+pnorm(two.binom.test(cl2))
 
-# 2. unemployed; recid ~ arrest
+
+# 3. all; re-abuse ~ arrest
+# among all subjects
+# H0: the occurrence of a subsequent assault is the same between the two groups
+# HA: the re-abuse rate is different in the non-arrest group from the treatment group
+cl3 <- cl1 + cl2
+cl3
+fisher.test(cl3, alternative="two.sided")$p.value
+pnorm(two.binom.test(cl3)) / 2
 
 
-
-# 3. recid ~ arrest
 
