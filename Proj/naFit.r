d268e6cb8a4261157ed9e45629c0f18a93374f42
Ying Qiao <yingqiao@eecs.berkeley.edu>
2013-04-20 20:58:34 -0700

    interpolation done

--- Proj/naFit.r
+++ Proj/naFit.r
@@ -8,24 +8,27 @@ notNA <- as.numeric(names(dieTT)[which(dieTT == max(dieTT))])
 nna <- lw.id(notNA[2])
 nna.x <- ibm[ibm$lot == nna$l & ibm$wafer == nna$w, 'chipX']
 nna.y <- ibm[ibm$lot == nna$l & ibm$wafer == nna$w, 'chipY']
+i=1;j=1;
 
 meas <- grep("psro", names(ibm))
 sill <- matrix(0, nrow=length(unique(id)), ncol=length(meas))
-for (i in 1:length(wid)){
-  for (j in 1:length(meas)){
+#for (i in 1:length(wid)){
+#  for (j in 1:length(meas)){
     gid <- lw.id(wid[i])
     gR <- ibm[ibm$lot == gid$l & ibm$wafer == gid$w,  ]
     gR <- as.geodata(gR, coords.col=c('chipX','chipY'), data.col=meas[j])
     vario <- variog(gR, estimate='modulus')
     var.mod <- variofit(vario, c(max(vario$v),max(vario$u)), cov.model='cubic')
     sill[i, j] <- var.mod$cov.pars[1]
-  }
-}
+#  }
+#}
+load('sill.Rdata')
 cid <- which(sill == 0, arr=T)
 table(cid[  ,1])
 
+
 sill2 <- rep(0, length(wid))
-for (i in 1:length(wid)){
+#for (i in 1:length(wid)){
   gid <- lw.id(wid[i])
   gR <- ibm[ibm$lot == gid$l & ibm$wafer == gid$w,  ]
   dieMeans <- rowMeans(gR[  ,meas])
@@ -40,11 +43,12 @@ for (i in 1:length(wid)){
 #  jpeg(file=fn)
 #  plot(vario,main='Variogram');lines(var.mod)
 #  dev.off()
-}
-wid[which(sill2 == 0)]
-
-sort(table(cid[cid[ ,1] %in% which(sill2 == 0), 1]))
+#}
+load('sill2.Rdata')
+table(cid[cid[ ,1] %in% which(sill2 == 0), 1])
 which(sill2 == 0)
+
+
 sill.est <- sill
 for (k in 1:dim(cid)[1]){
   if (sill2[cid[k,1]] != 0)
@@ -56,9 +60,49 @@ for (k in 1:dim(cid)[1]){
   }
 }
 which(sill.est == 0)
-sill2.est <- sill2
-sill2.est[sill2.est == 0] <- apply(sill.est[which(sill2 == 0), ], 1, median)
 
 
 
+
+
+
+wfgr <- data.frame(cbind(nna.x, nna.y))
+names(wfgr) <- c('chipX','chipY')
+rng <- min(max(nna.x),max(nna.y))
+nug <- 0
+
+ndie <- length(nna.x)
+IBMC <- data.frame(matrix(0, nrow=length(wid)*ndie,
+                          ncol=ncol(ibm)))
+names(IBMC) <- names(ibm)
+
+for (i in 1:length(wid)){
+  gid <- lw.id(wid[i])
+  wR <- ibm[ibm$lot == gid$l & ibm$wafer == gid$w,  ]
+  rowl <- (1:ndie) + (i-1)*ndie
+  IBMC[rowl, 'lot_ID'] <- gid$l
+  IBMC[rowl, 'wafer_ID'] <- gid$w
+  IBMC[rowl, c('chipX','chipY')] <- wfgr
+  for (j in 1:length(meas)){    
+    gR <- as.geodata(wR, coords.col=c('chipX','chipY'), data.col=meas[j])
+    ctrl <- krige.control(type.krige='ok',cov.model='cubic',
+                          cov.pars=c(sill[i,j],rng),nugget=nug)
+    krg.mod <- krige.conv(gR, locations=wfgr, krige=ctrl)
+    intpol <- krg.mod$pred
+    IBMC[rowl, meas[j]] <- intpol
+  }
+}
+write.csv(IBMC, file='IBMC.csv')
+
+
+## wafer meas heatmap
+plotWmap <- function(dm, id){
+  title <- sprintf("Lot%dWafer%d", id$l, id$w)
+  fig <- ggplot(dm, aes(x = chipX, y = chipY)) + scale_x_continuous(breaks=1:13) + scale_y_continuous(breaks=1:13)
+  fig <- fig + geom_point(aes(colour = intpol),shape = 15,size = 10) + ggtitle(title)
+  fig <- fig + guides(colour = guide_colorbar(barwidth = 0.5, barheight = 20))
+  fig
+}
+dm <- data.frame(cbind(wfgr,intpol))
+plotWmap(dm, gid)
 
--- Proj/outlierTrue.r
+++ Proj/outlierTrue.r
@@ -7,29 +7,25 @@ plotWmap <- function(dm, id){
   title <- sprintf("Lot%dWafer%d", id$l, id$w)
   fn <- sprintf("%s.jpeg",title)
   fig <- ggplot(dm, aes(x = chipX, y = chipY)) + scale_x_continuous(breaks=1:13) + scale_y_continuous(breaks=1:13)
-  fig <- fig + geom_point(aes(colour = dieSD),shape = 15,size = 10) + ggtitle(title)
+  fig <- fig + geom_point(aes(colour = dieMean),shape = 15,size = 10) + ggtitle(title)
   fig <- fig + guides(colour = guide_colorbar(barwidth = 0.5, barheight = 20))
   fig
 #  ggsave(filename=fn, plot=fig)
 }
 
-
-ibm$idno <- ibm$lot_ID*100 + ibm$wafer_ID
-for (mi in unique(ibm$idno)){
-mi=707
-nna <- lw.id(mi)
-plotid <- nna
-wwData <- ibm[ibm$lot == nna$l & ibm$wafer == nna$w,  ]
-dieMean<-rowMeans(wwData[  ,grep("psro", names(ibm))])
-dieSD <-rowSums((wwData[  ,grep("psro", names(ibm))] - dieMean)^2)/14
-dm <- cbind(wwData[  ,c('chipX','chipY')], dieSD) #dieMean, dieSD
-plotWmap(dm, nna)
-#}
+wid <- unique(id)
+for (mi in wid){
+  pid <- lw.id(mi)
+  wwData <- ibm[ibm$lot == pid$l & ibm$wafer == pid$w,  ]
+  dieMean<-rowMeans(wwData[  ,grep("psro", names(ibm))])
+  dm <- cbind(wwData[  ,c('chipX','chipY')], dieMean)
+  plotWmap(dm, pid)
+}
 
 
+outlierTrue <- c(421,507,523,707,1605,1607,2201,2205,508,703,421,
+                 1305,1412,1507,1604,1606,1207,1201,607,514,511,509,505,1909,1915,
+                 1705,517,520,521,1217,1311,1415,1901,1906,1911,503,1312,1413,515)
+outlierTrue <- sort(unique(outlierTrue))
+length(outlierTrue)
 
-
-
-
-ll <- c(707,523,2205,301,1008,801,1005,2201,1607,914,
-        1619,1620,2316,807,808,911,1512,1513,1605,1617,1618,2306,2307,2311,2314,2315)
